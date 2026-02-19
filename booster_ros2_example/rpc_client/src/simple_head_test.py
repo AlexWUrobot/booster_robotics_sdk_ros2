04bad875ee4c9a5bd042409cb65a4246ab787d84
LiFan <keystoradio@gmail.com>
2026-02-18 17:37:51 -0800

    change angles

--- booster_ros2_example/rpc_client/src/simple_head_test.py
+++ booster_ros2_example/rpc_client/src/simple_head_test.py
@@ -27,7 +27,7 @@ def main():
 
     # Look down
     node.get_logger().info('Looking down (-1.0)...')
-    req_look_down = create_msg(2004, {"pitch": -1.0, "yaw": 0.0})
+    req_look_down = create_msg(2004, {"pitch": 0.5, "yaw": 0.0})
     request = RpcService.Request()
     request.msg = req_look_down
     future = client.call_async(request)
@@ -41,7 +41,7 @@ def main():
 
     # Look up
     node.get_logger().info('Looking up (1.0)...')
-    req_look_up = create_msg(2004, {"pitch": 1.0, "yaw": 0.0})
+    req_look_up = create_msg(2004, {"pitch": -0.5, "yaw": 0.0})
     request.msg = req_look_up
     future = client.call_async(request)
     rclpy.spin_until_future_complete(node, future)
@@ -67,7 +67,7 @@ def main():
 
     # Look right
     node.get_logger().info('Looking right (yaw -1.0)...')
-    req_look_right = create_msg(2004, {"pitch": 0.0, "yaw": -1.0})
+    req_look_right = create_msg(2004, {"pitch": 0.0, "yaw": -0.5)
     request.msg = req_look_right
     future = client.call_async(request)
     rclpy.spin_until_future_complete(node, future)
